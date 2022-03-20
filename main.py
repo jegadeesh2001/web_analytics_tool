b7d3620445e9f263c424cff5c743f42c8ee15327
jegadeesh2001 <jegadeesh.sara@gmail.com>
2022-03-20 20:06:31 +0530

    Added User Statistics

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import math
+import matplotlib.pyplot as plt
+import numpy as np
 from datetime import datetime,date
 
 
@@ -34,7 +37,7 @@ def error(df):
     df_server = df_er.groupby('Request').size()
     st.header("404 Code End Points")
     df_3 = pd.DataFrame({'Request': df_server.index, 'count': df_server.values})
-    fig3 = px.pie(df_3, names=df_3["Request"], values=df_3["count"], title="Frequent Error pages")
+    fig3 = px.pie(df_3, names=df_3["Request"], values=df_3["count"])
     st.plotly_chart(fig3)
 
     df_4=df_er.groupby('Dates').size()
@@ -46,19 +49,30 @@ def error(df):
 
 
 def popular_pages(df):
-    st.title('Top 10 Popular pages')
-    df_req = df.groupby('Request').size()
-    df_4 = pd.DataFrame({'Request': df_req.index, 'count': df_req.values})
-    df_4_sort=df_4.sort_values(by='count',ascending=False)
-    df_4_res=df_4_sort[:10]
-    fig4 = px.bar(df_4_res, x="Request", y="count", title="Popular pages",width=1000,height=800)
+    st.title('USER STATISTICS')
+    df_req = df.groupby('Methods').size()
+    df_4 = pd.DataFrame({'Methods': df_req.index, 'Count': df_req.values})
+    df_4_sort=df_4.sort_values(by='Count',ascending=False)
+
+    st.header("Most Used Methods by Users")
+    fig4 = px.bar(df_4_sort, x="Methods", y="Count")
     st.plotly_chart(fig4)
 
+    df_pag= df.groupby('Request').size()
+    df_5 = pd.DataFrame({'Web page': df_pag.index, 'Count': df_pag.values})
+    df_5_sort = df_5.sort_values(by='Count', ascending=False)
+    df_5_res=df_5_sort[:10]
+    st.header("Popular Web Pages")
+    fig5 = px.bar(df_5_res, x='Web page', y="Count",height=800,width=1000)
+    st.plotly_chart(fig5)
 
-
-
-
-
+    df_st = df.groupby('Status_code').size()
+    df_6 = pd.DataFrame({'Status Code': df_st.index, 'Count': df_st.values})
+    df_6_sort = df_6.sort_values(by='Count', ascending=False)
+    #df_6_res = df_5_sort[:10]
+    st.header("Most Returned Status Code for Users")
+    fig6 = px.bar(df_6_sort, x='Status Code', y="Count",height=500,width=800)
+    st.plotly_chart(fig6)
 
 
 
@@ -70,13 +84,16 @@ def visitor_length(df):
 
     user_1_data.sort_values('datetime',axis=0,ascending=True,inplace=True)
     st.write(user_1_data)
-    today = date.today()
-    d_end = datetime.datetime.combine(today,df.iloc[-1]['Time'])
-    d_start = datetime.datetime.combine(today, df.iloc[0]['Time'])
 
-    len=d_end-d_start
+    end=user_1_data.iloc[-1]['datetime']
+    start=user_1_data.iloc[0]['datetime']
 
-    st.subheader("Time spent by the user in the website: "+ str(len))
+
+    diff=end-start
+
+    st.write("Starting Time: "+str(start))
+    st.write("Ending Time: "+str(end))
+    st.subheader("Time spent by the user in the website is "+ str(math.floor(diff.seconds/3600))+" hours")
 
 
 
@@ -85,7 +102,7 @@ def main():
     st.title("WEB ANALYTICS TOOL")
 
 
-    menu = ["Frequent Visitors","Visitor Length","404 Codes","Popular Pages"]
+    menu = ["Frequent Visitors","Visitor Length","404 Codes","User Statistics"]
     choice = st.sidebar.selectbox("Metrics", menu)
     header_list = ['IP', 'Timestamp', 'Request', 'Status_code']
     uploaded_file = st.file_uploader("Choose a CSV file")
@@ -103,12 +120,21 @@ def main():
         # if data['URL'].str.contains('.js').any():
         #   data['URL_new'] = data['URL'].str.split('/').str[3]
 
+        df['Methods'] = df['Request'].str.split('/').str[0]
+        if df['Request'].str.contains('.php').any():
+            df['URL_new'] = df['Request'].str.split('/').str[1]
+        elif df['Request'].str.contains('.js').any():
+            df['URL_new'] = df['Request'].str.split('/').str[3]
+        df['URL_new'] = df['URL_new'].str.split('HTTP')
+
         from datetime import datetime
         df["datetime"] = df["Timestamp"].apply(lambda x: datetime.strptime(x, "[%d/%b/%Y:%H:%M:%S"))
         #df["Timestamp"] = pd.to_datetime(df["Timestamp"])
 
         df['Dates'] = pd.to_datetime(df['datetime']).dt.date
         df['Time'] = pd.to_datetime(df['datetime']).dt.time
+
+
 
         if choice == "Frequent Visitors":
                 visitor_df(df)
@@ -119,20 +145,8 @@ def main():
         if choice=="404 Codes":
             error(df)
 
-        if choice=="Popular pages":
+        if choice=="User Statistics":
             popular_pages(df)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
